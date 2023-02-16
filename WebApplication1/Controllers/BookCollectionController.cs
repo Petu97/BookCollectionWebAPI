5309dd1a6acf5a0cec2a1444869fa5fc214f4b9b
Petteri <petu_97@hotmail.com>
2023-02-16 20:55:32 +0200

    Added BookController with basic methods

---
+++ WebApplication1/Controllers/BookCollectionController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Json.Nodes;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+
+    [ApiController]
+    [Route("BookCollection")]
+    public class BookCollectionController : Controller
+    {
+        [HttpGet("books")]
+        public IActionResult GetBooks(int ?id)
+        {
+            return Ok();
+        }
+
+        [HttpPost("books")]
+        [ValidateAntiForgeryToken]
+        public IActionResult CreateBook(JsonObject newBook)
+        {
+            return Ok();
+        }
+
+        [HttpDelete("books")]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeleteBook(int id)
+        {
+            return Ok();
+        }
+    }
+}
