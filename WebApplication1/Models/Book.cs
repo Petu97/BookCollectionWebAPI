fa1d4b28d126cfd13c11e9e5647225ebe01526d7
Petteri <petu_97@hotmail.com>
2023-02-16 20:54:57 +0200

    Added folder for models and a new model file for books

---
+++ WebApplication1/Models/Book.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+
+namespace WebApplication1.Models
+{
+    public class Book
+    {
+        [JsonPropertyName("id")]
+        public int Id { get; set; }
+
+        [JsonPropertyName("title")]
+        [Required]
+        public string Title { get; set; }
+
+        [JsonPropertyName("author")]
+        [Required]
+        public string Author { get; set; }
+
+        [JsonPropertyName("year")]
+        [Required]
+        public int Year { get; set; }
+
+        [JsonPropertyName("publisher")]
+        public string Publisher { get; set; }
+
+        [JsonPropertyName("description")]
+        public string Description { get; set; }
+
+    }
+}
