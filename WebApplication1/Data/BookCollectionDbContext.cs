bca40301ba11fb3a47615b0f6f660cb003d2612f
Petteri <petu_97@hotmail.com>
2023-02-20 19:07:20 +0200

    Added database context file

---
+++ WebApplication1/Data/BookCollectionDbContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using WebApplication1.Models;
+
+namespace WebApplication1.Data
+{
+    public class BookCollectionDbContext : DbContext
+    {
+        protected readonly IConfiguration configuration;
+
+        public BookCollectionDbContext(IConfiguration _configuration)
+        {
+            configuration = _configuration;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            // connect to sqlite database
+            options.UseSqlite(configuration.GetConnectionString("BookDb"));
+        }
+
+        public DbSet<Book> books { get; set; }
+
+    }
+}
